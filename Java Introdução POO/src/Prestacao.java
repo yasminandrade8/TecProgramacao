ef5c731ef3ecf0db079b4844203c5f8184173bb8
Yasmin Rodrigues de Andrade <yasminrandrade014@gmail.com>
2026-03-11 19:07:34 -0300

    Add files via upload

---
+++ Java Introdução POO/src/Prestacao.java
@@ -0,0 +1,23 @@
+//Objetivo: Calcular e mostrar o valor de uma prestação em atraso.
+
+import java.util.Scanner;
+
+public class Prestacao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double valor;
+        double taxa;
+        int tempo;
+        System.out.println("Valor da prestação: ");
+        valor = sc.nextDouble();
+        System.out.println("Taxa de juros: ");
+        taxa = sc.nextDouble();
+        System.out.println("Tempo: ");
+        tempo = sc.nextInt();
+
+        double Prestacao = valor + (valor *(taxa / 100) * tempo);
+
+        System.out.println("Sua prestação é de: " + Prestacao + " reais");
+        sc.close();
+    }
+}
